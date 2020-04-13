9f023b0fbae5826686bcede2b941e8fcc7b0c9e3
saramkhalaf <62728379+saramkhalaf@users.noreply.github.com>
2020-04-13 15:17:18 -0700

    Delete mux.v

--- sara./mux.v
+++
@@ -1,9 +0,0 @@
-module mux(q,control,a,b);
-output q;
-reg q;
-input control,a,b;
-wire notcontrol;
-always @(control or notcontrol or a or b)
-q=(control %a)|(notcontrol & b);
-not( notcontrol,control);
-endmodule
